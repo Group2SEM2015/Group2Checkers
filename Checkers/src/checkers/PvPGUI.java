bbf9a7376819a7e217dfafa9c364c7ea6f77b82f
bmp18 <bmp18@students.uwf.edu>
2015-10-28 15:59:45 -0500

    GUI and image
    added and image of checker board and changed/added GUI items

--- Checkers/src/checkers/PvPGUI.java
+++ Checkers/src/checkers/PvPGUI.java
@@ -5,11 +5,17 @@ import javax.swing.*;
 
 public class PvPGUI {
     public JFrame pvpGUI;
-    
+    public JPanel board = new JPanel();
+    public JLabel pic = new JLabel();
     public PvPGUI(){
         pvpGUI = new JFrame("Player Vs. Player");
         pvpGUI.setSize(900,700);
         pvpGUI.setVisible(true);
+        
+        ImageIcon icon = new ImageIcon(new ImageIcon("Images/checkers_board.png").getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
+        pic.setIcon(icon);
+        board.add(pic);
+        pvpGUI.add(board);
         pvpGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- Checkers/src/checkers/TutorialGUI.java
+++ Checkers/src/checkers/TutorialGUI.java
@@ -1,28 +1,44 @@
 package checkers;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class TutorialGUI {
     public JFrame tutorial = new JFrame("Tutorial Window");
     public JPanel board = new JPanel();
+    public JLabel pic = new JLabel();
+    public JPanel buttonPanel = new JPanel();
     public JTextField tips = new JTextField("Tips will go here");
     public JMenuBar menuBar = new JMenuBar();
-    public JMenu menu = new JMenu();
+    public JMenu menu = new JMenu("Tutorial Steps");
+    public JMenuItem steps = new JMenuItem("List of Tutorial steps to come");
     public JButton next = new JButton("Next");
     public JButton previous = new JButton("Previous");
     public TutorialGUI (){
+       ImageIcon icon = new ImageIcon(new ImageIcon("Images/checkers_board.png").getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
+       pic.setIcon(icon);
+        
         tutorial.setSize(900,700);
         tutorial.setVisible(true);
         tutorial.setLayout(new BorderLayout());
-        board.setBackground(Color.GREEN);
         menuBar.add(menu);
+        menu.add(steps);
+        board.add(pic);
         tutorial.add(board, BorderLayout.CENTER);
-        board.setBackground(Color.GREEN);
         tutorial.add(tips, BorderLayout.WEST);
-        tutorial.add(next, BorderLayout.SOUTH);
-        tutorial.add(previous, BorderLayout.SOUTH);
+        buttonPanel.setLayout(new GridLayout(1,2));
+        tutorial.add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.add(previous);
+        buttonPanel.add(next);
+        tutorial.add(menuBar, BorderLayout.NORTH);
+        
+        
         tutorial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 
 }
