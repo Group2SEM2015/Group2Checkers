eb4b498865d52b8d2dbe7aad6000b332e78016be
bmp18 <bmp18@students.uwf.edu>
2015-11-15 19:03:48 -0600

    Board Change
    Change Xavier came up with

--- Checkers/src/checkers/TutorialGUI.java
+++ Checkers/src/checkers/TutorialGUI.java
@@ -9,8 +9,10 @@ import javax.swing.*;
 
 public class TutorialGUI {
     public JFrame tutorial = new JFrame("Tutorial Window");
-    public JPanel board = new JPanel();
-    public JLabel pic = new JLabel();
+    public JPanel[][] checkerBoard1;
+    public JPanel checkerBoardPanel = new JPanel();
+    public JPanel redPanel;
+    public JPanel blackPanel;
     public JPanel buttonPanel = new JPanel();
     public JTextField tips = new JTextField("Tips will go here");
     public JMenuBar menuBar = new JMenuBar();
@@ -18,35 +20,25 @@ public class TutorialGUI {
     public JMenuItem steps = new JMenuItem("List of Tutorial steps to come");
     public JButton next = new JButton("Next");
     public JButton previous = new JButton("Previous");
-    public JPanel boardPanel = new JPanel();
     
-    
-    public TutorialGUI (){
-        //sets image to JLabel
-       ImageIcon icon = new ImageIcon(new ImageIcon("Images/checkers_board.png").getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
-       pic.setIcon(icon);
-        
-        tutorial.setSize(900,700);
+    public TutorialGUI()
+    {
+        tutorial.setSize(900, 700);
         tutorial.setVisible(true);
         tutorial.setLayout(new BorderLayout());
+
         menuBar.add(menu);
         menu.add(steps);
-        board.add(pic);
-        tutorial.add(board, BorderLayout.CENTER);
-        boardPanel.setSize(700,600);
-        
-        boardPanel.setLayout(new GridLayout(8,8));
-        
-        tutorial.add(boardPanel, BorderLayout.CENTER);
+        createCheckerBoard();
+
+        tutorial.add(checkerBoardPanel, BorderLayout.CENTER);
         tutorial.add(tips, BorderLayout.WEST);
-        buttonPanel.setLayout(new GridLayout(1,2));
+        buttonPanel.setLayout(new GridLayout(1, 2));
         tutorial.add(buttonPanel, BorderLayout.SOUTH);
         buttonPanel.add(previous);
         buttonPanel.add(next);
         tutorial.add(menuBar, BorderLayout.NORTH);
-        
-        
-        //tutorial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         tutorial.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         tutorial.addWindowListener(new java.awt.event.WindowAdapter()
@@ -57,6 +49,33 @@ public class TutorialGUI {
                 CheckersGUI gui1 = new CheckersGUI();
                 tutorial.dispose();
             }
-        });      
+        });
+    }
+
+    public void createCheckerBoard()
+    {
+        checkerBoard1 = new JPanel[8][8];
+        checkerBoardPanel.setLayout(new GridLayout(8, 8));
+        for (int i = 0; i < 8; i++)
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                if ((k + i) % 2 == 0)
+                {
+                    redPanel = new JPanel();
+                    redPanel.setBackground(Color.red);
+                    redPanel.setVisible(true);
+                    checkerBoard1[i][k] = (redPanel);
+                }
+                else
+                {
+                    blackPanel = new JPanel();
+                    blackPanel.setBackground(Color.black);
+                    blackPanel.setVisible(true);
+                    checkerBoard1[i][k] = (blackPanel);
+                }
+                checkerBoardPanel.add(checkerBoard1[i][k]);
+            }
+        }
     }
 }
