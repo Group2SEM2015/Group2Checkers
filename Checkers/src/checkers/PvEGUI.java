5634f4df879802da854d8be9801a16b2f17ab0b3
bmp18 <bmp18@students.uwf.edu>
2015-11-18 19:02:54 -0600

    Add Steps in the Tutorial
    Made the steps to the tutorial that we will need later as well as made
    the board again for the PVP and PVE

--- Checkers/src/checkers/PvEGUI.java
+++ Checkers/src/checkers/PvEGUI.java
@@ -5,17 +5,23 @@ import javax.swing.*;
 
 public class PvEGUI {
     public JFrame pveGUI;
-    public JPanel board = new JPanel();
-    public JLabel pic = new JLabel();
+    //public JPanel board = new JPanel();
+    //public JLabel pic = new JLabel();
+    public JPanel[][] checkerBoard1;
+    public JPanel checkerBoardPanel = new JPanel();
+    public JPanel redPanel;
+    public JPanel blackPanel;
     public PvEGUI(){
         pveGUI = new JFrame("Player Vs. Computer");
         pveGUI.setSize(900,700);
         pveGUI.setVisible(true);
+        pveGUI.setLayout(new BorderLayout());
+        createCheckerBoard();
         //sets image to JLabel
-        ImageIcon icon = new ImageIcon(new ImageIcon("Images/checkers_board.png").getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
-        pic.setIcon(icon);
-        board.add(pic);
-        pveGUI.add(board);
+        //ImageIcon icon = new ImageIcon(new ImageIcon("Images/checkers_board.png").getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
+        //pic.setIcon(icon);
+        //board.add(pic);
+        pveGUI.add(checkerBoardPanel, BorderLayout.CENTER);
         //pveGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pveGUI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     
@@ -28,5 +34,31 @@ public class PvEGUI {
                 pveGUI.dispose();
             }
         });
+    }
+      public final void createCheckerBoard()
+    {
+        checkerBoard1 = new JPanel[8][8];
+        checkerBoardPanel.setLayout(new GridLayout(8, 8));
+        for (int i = 0; i < 8; i++)
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                if ((k + i) % 2 == 0)
+                {
+                    redPanel = new JPanel();
+                    redPanel.setBackground(Color.red);
+                    redPanel.setVisible(true);
+                    checkerBoard1[i][k] = (redPanel);
+                }
+                else
+                {
+                    blackPanel = new JPanel();
+                    blackPanel.setBackground(Color.black);
+                    blackPanel.setVisible(true);
+                    checkerBoard1[i][k] = (blackPanel);
+                }
+                checkerBoardPanel.add(checkerBoard1[i][k]);
+            }
+        }
     }
 }
--- Checkers/src/checkers/TutorialGUI.java
+++ Checkers/src/checkers/TutorialGUI.java
@@ -1,26 +1,31 @@
 package checkers;
 import java.awt.*;
 import java.awt.event.*;
-//import java.awt.image.*;
-//import java.io.File;
-//import java.io.IOException;
-//import javax.imageio.ImageIO;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class TutorialGUI {
     public JFrame tutorial = new JFrame("Tutorial Window");
+    int i = 1;
     public JPanel[][] checkerBoard1;
     public JPanel checkerBoardPanel = new JPanel();
     public JPanel redPanel;
     public JPanel blackPanel;
     public JPanel buttonPanel = new JPanel();
-    public JTextField tips = new JTextField("Tips will go here");
+    public JTextArea tips = new JTextArea();
     public JMenuBar menuBar = new JMenuBar();
     public JMenu menu = new JMenu("Tutorial Steps");
-    public JMenuItem steps = new JMenuItem("List of Tutorial steps to come");
+    public JMenuItem step1 = new JMenuItem("Move");
+    public JMenuItem step2 = new JMenuItem("Jump");
+    public JMenuItem step3 = new JMenuItem("Making a King");
+    public JMenuItem step4 = new JMenuItem("Winning");
+    //public JMenuItem steps = new JMenuItem("List of Tutorial steps to come");
     public JButton next = new JButton("Next");
     public JButton previous = new JButton("Previous");
-    public boolean draw = false;
+    
     public TutorialGUI()
     {
         tutorial.setSize(900, 700);
@@ -28,7 +33,10 @@ public class TutorialGUI {
         tutorial.setLayout(new BorderLayout());
 
         menuBar.add(menu);
-        menu.add(steps);
+        menu.add(step1);
+        menu.add(step2);
+        menu.add(step3);
+        menu.add(step4);
         createCheckerBoard();
 
         tutorial.add(checkerBoardPanel, BorderLayout.CENTER);
@@ -38,6 +46,15 @@ public class TutorialGUI {
         buttonPanel.add(previous);
         buttonPanel.add(next);
         tutorial.add(menuBar, BorderLayout.NORTH);
+        tips.setEditable(false);
+        tips.setText("Hello and Welcome to the wonderfull\n"
+                + "world ofcheckers. In this tutorial we \n"
+                + "will help youunderstand the basic \n"
+                + "mechanics of this timless game. \n"
+                + "Let's get started shall we!\n"
+                + "Please Clicked the next button down below\n"
+                + "or select the step you wish to learn in\n"
+                + "the menu above");
 
         tutorial.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -48,6 +65,89 @@ public class TutorialGUI {
             {
                 CheckersGUI gui1 = new CheckersGUI();
                 tutorial.dispose();
+            }
+        });
+        //switch the boardpanel and text box to the move step
+        step1.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+               tips.setText("In this step you will learn the\n"
+                       + "basic move function this allows you\n "
+                       + "move your checker piece(Red) to a new\n"
+                       + "square.\n"
+                       + "To do this please click on the the\n"
+                       + "highlighted checker piece then\n"
+                       + "please drage this piece to the highlighter\n "
+                       + "square. This is the move function.\n"
+                       + "The goal to moving pieces is to set\n"
+                       + "them up in postions to allow you to\n "
+                       + "jump the other players pieces");
+
+           } 
+        });
+        //switch the boardpanel and text box to the jump step
+        step2.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+               tips.setText("In this step you will learn the\n"
+                       + "jump function. The jump allows the\n"
+                       + "player to take his/hers oppenets\n"
+                       + "piece. To demostrate this we have set\n"
+                       + "up the board that allows you jump your\n"
+                       + "oppenet. Please select the highlighted\n"
+                       + "checker piece. And move the checker\n"
+                       + "piece to the highighted square.\n"
+                       + "As you can see with the jump you\n"
+                       + "are able to move over the oppenets\n"
+                       + "piece and take it. You can only do this\n"
+                       + "if the space behind the oppents piece\n"
+                       + "However do becareful, you do not want\n"
+                       + "to setup your peice to get jump after.");
+          }
+        });
+        //switch the boardpanel and text box to the king step
+        step3.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+               tips.setText("In this step you will learn what\n"
+                       + "a king is and what you can do with\n"
+                       + "one. The king is a checker peice that\n"
+                       + "has reached the oppisite side of the\n"
+                       + "board. When this happens another piece\n"
+                       + "is placed on top of the one that reached\n"
+                       + "the end. This allows the checker piece to\n"
+                       + "move in any forward or back.\n"
+                       + "This piece can dominate the field.\n"
+                       + "Please select the higlighted piece and\n"
+                       + "drag it to the highlighted box. As you\n"
+                       + "can see the piece has changed.\n"
+                       + "Now move the peice to the previos spot\n"
+                       + "you where just at.\n"
+                       + "This is called multidirectional movement.\n"
+                       + "This will allow you to set up new jump\n"
+                       + "options and a faster way to bring down your\n"
+                       + "oppenet.");
+               
+           } 
+        });
+        step4.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+                 tips.setText("In this step we will cover winning.\n"
+                         + "To win at checkers you have to take all\n"
+                         + "of your oppenets pieces. We have set up\n"
+                         + "the board so all that is left is one piece\n"
+                         + "on your oppenets side.\n"
+                         + "Select the highlighted peice and jump the\n"
+                         + "the final oppenets piece.\n"
+                         + "This is the game of Checkers a critical\n"
+                         + "thinking game that makes you think steps\n"
+                         + "in advance to assure your WIN!");
+             }
+        });
+        next.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+              
             }
         });
     }
@@ -77,14 +177,7 @@ public class TutorialGUI {
                 checkerBoardPanel.add(checkerBoard1[i][k]);
             }
         }
-       //checkerBoard1[0][1];
     }
-    /*
-    Draws Checker piece
-     public void paint(Graphics g) {
-        Graphics2D ga = (Graphics2D)g;
-        ga.setPaint(Color.red);
-        ga.fillOval(50,50,25,25);
-  }
-    */
+
+ 
 }
